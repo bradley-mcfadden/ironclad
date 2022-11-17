a9ac4b4122b2782d357a5530420497f15703af5d
Bradley McFadden <student.rd2@jannatec.com>
2022-11-17 11:15:17 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,179 @@
+use std::ops::Add;
+
+const EMPTY_PLAYER_ID: i32 = -1;
+const PLAYER_A_ID: i32 = 1;
+const PLAYER_B_ID: i32 = 2;
+
+struct Game {
+    board: Board,
+    players: [Player; 2], 
+}
+
+impl Game {
+    
+}
+
+struct Player {
+    id: i32,
+    stones: i32,
+}
+
+impl Player {
+    fn new(_id: i32, nstones: i32) -> Player {
+        Player {
+            id: _id,
+            stones: nstones
+        }
+    }
+}
+
+const BOARD_WIDTH: usize = 6;
+const BOARD_HEIGHT: usize = 8;
+
+struct Board {
+    checker_board: [Checker; BOARD_WIDTH * BOARD_HEIGHT],
+    stone_board: [Stone; (BOARD_WIDTH + 1) * (BOARD_HEIGHT + 1)]
+}
+
+impl Board {
+    fn new() -> Board {
+        let mut board = Board {
+            checker_board: [Checker{height: 0, owner: EMPTY_PLAYER_ID}; BOARD_WIDTH * BOARD_HEIGHT],
+            stone_board: [Stone{owner: EMPTY_PLAYER_ID}; (BOARD_WIDTH + 1) * (BOARD_HEIGHT + 1)] 
+        };
+        board.place_checker_at(Vec2::new(6, 2), Checker::new(1, PLAYER_A_ID));
+        board.place_checker_at(Vec2::new(6, 3), Checker::new(1, PLAYER_A_ID));
+        board.place_checker_at(Vec2::new(7, 1), Checker::new(2, PLAYER_A_ID));
+        board.place_checker_at(Vec2::new(7, 4), Checker::new(2, PLAYER_A_ID));
+        board.place_checker_at(Vec2::new(7, 3), Checker::new(3, PLAYER_A_ID));
+        board.place_checker_at(Vec2::new(7, 2), Checker::new(3, PLAYER_A_ID));
+
+        board.place_checker_at(Vec2::new(1, 2), Checker::new(1, PLAYER_B_ID));
+        board.place_checker_at(Vec2::new(1, 3), Checker::new(1, PLAYER_B_ID));
+        board.place_checker_at(Vec2::new(0, 1), Checker::new(2, PLAYER_B_ID));
+        board.place_checker_at(Vec2::new(0, 4), Checker::new(2, PLAYER_B_ID));
+        board.place_checker_at(Vec2::new(0, 3), Checker::new(3, PLAYER_B_ID));
+        board.place_checker_at(Vec2::new(0, 2), Checker::new(3, PLAYER_B_ID));
+        
+        board
+    }
+
+    fn can_move_checker(&self, from: Vec2, to: Vec2) -> bool {
+        true
+    }
+
+    fn place_checker_at(&mut self, pos: Vec2, checker: Checker) {
+        let idx = Board::vec_to_checker_idx(pos);
+        self.checker_board[idx] = checker
+    }
+
+    fn can_put_stone(&self, pos: Vec2) -> bool {
+        true
+    }
+
+    fn can_move_stone(&self, from: Vec2, to: Vec2) -> bool {
+        true
+    }
+
+    fn place_stone_at(&mut self, pos: Vec2, stone: Stone) {
+        let idx = Board::vec_to_stone_idx(pos);
+        self.stone_board[idx] = stone
+    }
+
+    fn fire_checker_at(&self, pos: Vec2) {}
+
+    fn can_fire_at(&self, from: Vec2, pos: Vec2) -> bool {
+        true
+    }
+
+    fn checker_at<'a>(&'a self, pos: Vec2) -> &'a Checker {
+        let idx: usize = Board::vec_to_checker_idx(pos); 
+        &self.checker_board[idx]
+    }
+
+    fn stone_at<'a>(&'a self, pos: Vec2) -> &'a Stone {
+        let idx: usize = Board::vec_to_stone_idx(pos);
+        &self.stone_board[idx]
+    }
+
+    fn vec_to_stone_idx(pos: Vec2) -> usize {
+        (pos.x + pos.y) as usize * (BOARD_HEIGHT + 1)
+    }
+
+    fn vec_to_checker_idx(pos: Vec2) -> usize {
+        (pos.x + pos.y) as usize * BOARD_HEIGHT
+    }
+
+}
+
+#[derive(Copy, Clone)]
+struct Vec2 {
+    x: i32,
+    y: i32
+}
+
+const UP: Vec2 = Vec2 {x:0, y:-1};
+const DOWN: Vec2 = Vec2 {x:0, y:1};
+const LEFT: Vec2 = Vec2 {x:-1, y:0};
+const RIGHT: Vec2 = Vec2 {x:1, y:0};
+
+impl Vec2 {
+    fn new(xi: i32, yi: i32) -> Vec2 {
+        Vec2{x: xi, y: yi}
+    }
+
+    fn left(self) -> Vec2 {
+        self + LEFT
+    }
+
+    fn right(self) -> Vec2 {
+        self + RIGHT
+    }
+
+    fn up(self) -> Vec2 {
+        self + UP
+    }
+
+    fn down(self) -> Vec2 {
+        self + DOWN
+    }
+}
+
+impl Add for Vec2 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Vec2 {
+            x: self.x + other.x,
+            y: self.y + other.y
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Checker {
+    height: i32,
+    owner: i32
+}
+
+impl Checker {
+    fn new(h: i32, o: i32) -> Checker {
+        Checker { height: h, owner: o }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Stone {
+    owner: i32
+}
+
+impl Stone {
+    fn new(o: i32) -> Stone {
+        Stone { owner: o }
+    }
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
